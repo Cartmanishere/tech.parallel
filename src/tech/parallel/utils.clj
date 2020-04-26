af2cb3149de9f0f35bb43b7835875cf0272952c5
pranav <pranav.gajjewar45@gmail.com>
2020-04-26 12:56:14 +0530

    Remove dependency on clj-1.10

--- src/tech/parallel/utils.clj
+++ src/tech/parallel/utils.clj
@@ -1,6 +1,25 @@
 (ns tech.parallel.utils)
 
 
+(defn qualified-symbol?
+  [x] (boolean (and (symbol? x) (namespace x) true)))
+
+
+(defn- serialized-require
+  [& args]
+  (locking (Object.)
+    (apply require args)))
+
+
+(defn requiring-resolve
+  [sym]
+  (if (qualified-symbol? sym)
+    (or (resolve sym)
+        (do (-> sym namespace symbol serialized-require)
+            (resolve sym)))
+    (throw (IllegalArgumentException. (str "Not a qualified symbol: " sym)))))
+
+
 (defmacro export-symbols
   [src-ns & symbol-list]
   `(do
